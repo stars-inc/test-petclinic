6a1dfecfede32e910f9db2b76a9fd65f4e148709
stars-inc <stars_inc@mail.ru>
2026-08-07 10:57:00 +0300

    feat: env && assured config

---
+++ src/test/java/com/example/demo/RestAssuredService.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RestAssuredService {
+  private final RequestSpecification requestSpecification;
+
+  public RestAssuredService(@Value("${petclinic.base-url}") String baseUrl) {
+    RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+    this.requestSpecification = new RequestSpecBuilder()
+      .setBaseUri(removeTrailingSlash(baseUrl))
+      .setContentType(ContentType.JSON)
+      .setAccept(ContentType.JSON)
+      .build();
+  }
+
+  public RequestSpecification spec() { return requestSpecification; }
+
+    private String removeTrailingSlash(String url) {
+      if (url == null || url.isBlank()) {
+        throw new IllegalArgumentException("baseUlr -> not be blank");
+      }
+
+        return url.endsWith("/") ? url.substring(0, url.length() - 1) : url;
+    }
+}
